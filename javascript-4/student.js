c2cc9145e5d644722778ae117c8761cbd01069c7
Jeordman <callisterjeordin@gmail.com>
2019-06-13 14:37:28 -0600

    yay

--- javascript-4/student.js
+++ javascript-4/student.js
@@ -5,9 +5,11 @@
 // Create a new array called 'faveColors' and set it your three favorite colors as strings.
 
 // CODE HERE
+const faveColors = ['aqua', 'black', 'blue']
 
 //////////////////Step 2////////////////////
-// Create an object called 'me' that has these keys: firstname, superHeroName, homeTown, superPowers, superPowerXP, profileImage. 
+// Create an object called 'me' that has these keys: firstname, superHeroName, homeTown,
+// superPowers, superPowerXP, profileImage. 
 
 // The firstName key should be your name as a string. 
 
@@ -22,11 +24,23 @@
 // The profileImage key will return this url `https://randomuser.me/api/portraits/med/lego/${Math.floor(Math.random() * 10) + 1 }.jpg`.
 
 // CODE HERE
+let me = {
+    firstname: 'Jeordin',
+    superHeroName: 'w33dg0d',
+    homeTown: 'West Valley',
+    superPowers: ['crying', 'weeping', 'pondering'],
+    superPowerXP: function() {
+       return Math.floor(Math.random() * 100) + 1 
+    },
+    profileImage: `https://randomuser.me/api/portraits/med/lego/${Math.floor(Math.random() * 10) + 1 }.jpg`
+}
 
 //////////////////Step 3////////////////////
-// Create three variables to hold some data off your me object. The first variable should be 'regularName' that is the value of your firstName on the me object. The next variable is 'superName' which is the value of superHeroName on the me object. Last is 'homeTown' which will be the value of homeTown on the me object
+// Create three variables to hold some data off your me object. 
+//The first variable should be 'regularName' that is the value of your firstName on the me object. The next variable is 'superName' which is the value of superHeroName on the me object. Last is 'homeTown' which will be the value of homeTown on the me object
 
 // CODE HERE
+
 
 //////////////////Step 4////////////////////
 //Create a function called 'setColor' that takes in one param called arr. Just to make sure that we only ever have three colors, use splice to trim the array to 3 colors. 
